8506cabd1d25c30b11b1c9902b0119349f933200
boris_montavon <boris.montavon@gmail.com>
2022-08-04 13:55:13 +0200

    [+] Project init

---
+++ src/component/DataTable/react-data-table.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./react-data-table.css";
+
+export interface ButtonProps {
+    label: string;
+}
+  
+const Button = ({label}: ButtonProps) => {
+    return <button>{label}</button>;
+};
+  
+export default Button;
